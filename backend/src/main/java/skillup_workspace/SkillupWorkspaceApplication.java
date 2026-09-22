97a2aa8a9d818d0ee87ca7ae82df435c42e1f70f
Yusleidy Gamboa <yusgamboa16@gmail.com>
2026-09-22 16:14:30 -0500

    feat(backend): initialize spring boot project structure and database config

---
+++ backend/src/main/java/skillup_workspace/SkillupWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package skillup_workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillupWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillupWorkspaceApplication.class, args);
+	}
+
+}
